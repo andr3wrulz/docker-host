14619aaf37b1e0adcb3cd0c0e6e28f67ac4b69b1
Andrew <geltz.andrew@gmail.com>
2018-08-29 10:28:34 -0400

    Added Restic backup script

---
+++ restic.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#This will run Restic backups and remove snapshots according to a policy
+
+LOG_FILE=/home/andr3w/logs/restic.log
+BACKUP_DIR=/home/andr3w/docker
+
+#Define a timestamp function
+timestamp() {
+date "+%b %d %Y %T %Z"
+}
+
+# insert timestamp into log
+printf "\n\n"
+echo "-------------------------------------------------------------------------------" | tee -a $LOG_FILE
+echo "$(timestamp): restic.sh started" | tee -a $LOG_FILE
+
+echo "Running backup: $(timestamp)" | tee -a $LOG_FILE
+# Run Backups
+restic backup $BACKUP_DIR
+
+echo "Executing retention policy: $(timestamp)" | tee -a $LOG_FILE
+# Remove snapshots according to policy
+# If run cron more frequently, might add --keep-hourly 24
+restic forget --keep-daily 1 --keep-weekly 7 --keep-monthly 6 --keep-yearly 5  | tee -a $LOG_FILE
+
+echo "Cleaning up repo: $(timestamp)" | tee -a $LOG_FILE
+# Remove unneeded data from the repository
+restic prune
+
+echo "Checking repo for errors: $(timestamp)"
+# Check the repository for errors
+restic check | tee -a $LOG_FILE
+
+# insert timestamp into log
+printf "\n\n"
+echo "-------------------------------------------------------------------------------" | tee -a $LOG_FILE
+echo "$(timestamp): restic.sh finished" | tee -a $LOG_FILE
